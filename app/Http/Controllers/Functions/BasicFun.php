73b592abba40bd4e5996c8fb5d13139a4ed01a32
Maycon <dqsmaycon2@gmail.com>
2020-04-15 11:06:45 -0300

    Ajuste

--- app/Http/Controllers/Functions/BasicFun.php
+++ app/Http/Controllers/Functions/BasicFun.php
@@ -7,7 +7,7 @@ $valor = $_GET[$request->valor];
 converterDinheiro($valor);
 
 function converterDinheiro($valor){
-    echo number_format($valor,2, '.', ',');
+    echo number_format($valor,3, '.', ',');
 }
 
 ?>
